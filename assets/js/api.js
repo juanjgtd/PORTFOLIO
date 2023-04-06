ccc6659344aa2de35966b8f0d56e74cf9a0dd45b
juanjgtd <106997662+juanjgtd@users.noreply.github.com>
2023-04-06 13:49:31 -0300

    Atualização de diretório do portfólio

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/juanjgtd/DIO-CURSO-JAVASCRIPT/main/Exerc%C3%ADcios/07%20-%20Porf%C3%B3lio/assets/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/juanjgtd/PORTFOLIO/main/assets/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
